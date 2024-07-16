259b9f6b226fffbd2d1f94859ce43208339c42e9
jair549 <jairbarreto549@gmail.com>
2024-07-16 11:01:41 -0300

    Creating component sections

---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,20 @@
+<header class="header-page">
+    <div class="banner-header">
+        <div class="content-header">
+            <img src="{{ asset('img/nivus.jpg') }}" alt="">
+        </div>
+    </div>
+    <div class="container-header">
+        <div class="bg-description-header"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="content-description-header">
+                        <h1 class="title-header">O seu novo Consórcio Volkswagen está aqui!</h1>
+                        <a href="" class="btn btn-page">Simule aqui</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,23 +13,9 @@
 
 <body>
 
-    <nav id="main-nav" class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    @include('sections.nav')
+
+    @include('sections.header')
 
     @vite('resources/js/app.js')
 </body>
