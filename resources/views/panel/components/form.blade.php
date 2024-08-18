461a835bb0d58125cde340d27a2e95d362c1175a
geocacio <cacio@gginformaticaweb.com>
2024-08-18 11:10:34 -0300

    ajusta o input image

--- resources/views/panel/components/form.blade.php
+++ resources/views/panel/components/form.blade.php
@@ -80,12 +80,73 @@
 
                 </div>
             @endforeach
+            
+        @elseif($column->type == 'file')
+            <div class="form-group">
+                <div class="custom-input-file">
+                    <label for="logo">{{ $column->label }}</label>
+                    <input type="{{ $column->type}}" name="{{ $column->name }}" accept="image/*" class="form-control" onchange="showTempFile(event, 'custom-input-file', 'container-temp-file')">
+                    <div class="container-temp-file">
+                        <button type="button" class="btn btn-toggle-file" onclick="toggleFile(event)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
+                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        @elseif($column->type == 'select')
+        
+            <div class="form-group">
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                <select class="form-control" name="{{ $column->name }}" id="{{ $column->name }}" {{ isset($column->required) && $update ? 'required' : '' }}>
+                    @foreach($column->options as $option)
+                        <option value="{{ $option->value }}">{{ $option->label }}</option>
+                    @endforeach
+                </select>
+            </div>
+        @elseif($column->type == 'checkbox')
+        
+            <div class="form-group">
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                @foreach($column->options as $option)
+                    <input class="form-control" type="checkbox" name="{{ $column->name }}" id="{{ $column->name }}" value="{{ $option->value }}" {{ isset($column->required) && $update ? 'required' : '' }}>
+                    <label for="{{ $column->name }}">{{ $option->label }}</label>
+                @endforeach
+            </div>
+        @elseif($column->type == 'radio')
+
+            <div class="form-group">
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                @foreach($column->options as $option)
+                    <input class="form-control" type="radio" name="{{ $column->name }}" id="{{ $column->name }}" value="{{ $option->value }}" {{ isset($column->required) && $update ? 'required' : '' }}>
+                    <label for="{{ $column->name }}">{{ $option->label }}</label>
+                @endforeach
+            </div>
+        @elseif($column->type == 'text')
+            
+            <div class="form-group">
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                <input class="form-control" type="{{ $column->type}}" name="{{ $column->name }}" id="{{ $column->name }}" {{ isset($column->required) && $update ? 'required' : '' }}>
+            </div>
+        @elseif($column->type == 'url')
+            <div class="form-group
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                <input class="form-control" type="{{ $column->type}}" name="{{ $column->name }}" id="{{ $column->name }}" {{ isset($column->required) && $update ? 'required' : '' }}>
+            </div>     
+
+        @else
+            <div class="form-group">
+                <label for="{{ $column->name }}">{{ $column->label }}</label>
+                <textarea class="form-control" name="{{ $column->name }}" id="{{ $column->name }}" {{ isset($column->required) && $update ? 'required' : '' }}></textarea>
+            </div>
         @endif
 
     @endforeach
-    <div class="form-group text-right">
-        <button class="btn btn-default" type="submit">Cadastrar</button>
-    </div>
+
+    <button type="submit">Cadastrar</button>
 </form>
 
 @include('panel.partials.fileScripts')
