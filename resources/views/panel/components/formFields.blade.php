2edccb6cfae6493aad97293be82085f8da3af979
geocacio <cacio@gginformaticaweb.com>
2024-08-22 19:56:47 -0300

    exibe a imagem que está cadastrada no banco

--- resources/views/panel/components/formFields.blade.php
+++ resources/views/panel/components/formFields.blade.php
@@ -22,9 +22,17 @@
 
                         <div class="form-group">
                             <div class="custom-input-file">
-                                <label for="logo">{{ $field['label'] }}</label>
-                                <input type="{{ $field['type']}}" name="{{ $field['name'] }}" accept="image/*" class="form-control" onchange="showTempFile(event, 'custom-input-file', 'container-temp-file')">
+                                <label for="logo">Logo</label>
+                                <input type="file" name="logo" accept="image/*" class="form-control" onchange="showTempFile(event, 'custom-input-file', 'container-temp-file')">
                                 <div class="container-temp-file">
+                                    @if(isset($currentField[0]['files']))
+                                    <img class="image" src="{{ asset('storage/' . $currentField[0]['files']['path']) }}" />
+                                    <button class="btn-delete" onclick="removeFile(event, 'container-temp-file', 'endereco/painel/my-page/delete-image/logo')">
+                                        <svg viewBox="0 0 24 24" style="fill: white; width: 20px; height: 20px;">
+                                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" style="stroke-width: 4px;"></path>
+                                        </svg>
+                                    </button>
+                                    @endif
                                     <button type="button" class="btn btn-toggle-file" onclick="toggleFile(event)">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
                                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
@@ -86,7 +94,7 @@
 
     @endforeach
 
-    <button type="submit">Cadastrar</button>
+    <button class="btn btn-default" type="submit">Cadastrar</button>
 </form>
 
 @include('panel.partials.fileScripts')
--- resources/views/panel/components/formMain.blade.php
+++ resources/views/panel/components/formMain.blade.php
@@ -23,11 +23,34 @@
             @continue
             
         @elseif($column['type'] == 'file')
+            @dd($column)
+            <div class="form-group">
+                <div class="custom-input-file">
+                    <label for="logo">Logo</label>
+                    <input type="file" name="logo" accept="image/*" class="form-control" onchange="showTempFile(event, 'custom-input-file', 'container-temp-file')">
+                    <div class="container-temp-file">
+                        @if($my_page->logo != '')
+                        <img class="image" src="{{ asset($apiUrl.'/storage/'.$my_page->logo) }}" />
+                        <button class="btn-delete" onclick="removeFile(event, 'container-temp-file', '{{$apiUrl}}/painel/my-page/delete-image/logo')">
+                            <svg viewBox="0 0 24 24" style="fill: white; width: 20px; height: 20px;">
+                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" style="stroke-width: 4px;"></path>
+                            </svg>
+                        </button>
+                        @endif
+                        <button type="button" class="btn btn-toggle-file" onclick="toggleFile(event)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
+                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
                 
-            <div class="form-group
+            <!-- <div class="form-group
                 <label for="{{ $column['name'] }}">{{ $column['label'] }}</label>
                 <input class="form-control" type="{{ $column['type']}}" name="{{ $column['name'] }}" id="{{ $column['name'] }}" {{ isset($column['required']) && $update ? 'required' : '' }}>
-            </div>
+            </div> -->
 
         @elseif($column['type'] == 'select')
         
