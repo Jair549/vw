0f1d24590ac62d10d917e1db9f9deb4a3e3133fe
geocacio <cacio@gginformaticaweb.com>
2024-08-22 22:26:56 -0300

    remove rotas antigas

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('sections', SectionController::class);
-// Route::get('sections/{section}/create', [SectionController::class, 'create'])->name('sections.create');
-// Route::post('sections', [SectionController::class, 'store'])->name('sections.store');
-// Route::get('sections/{section}/edit', [SectionController::class, 'edit'])->name('sections.edit');
-Route::get('sections/{section}/edit/{id}', [SectionController::class, 'editField'])->name('sections.editField');
-// Route::put('sections/{section}', [SectionController::class, 'update'])->name('sections.update');
-Route::put('sections/{section}/edit/{id}', [SectionController::class, 'updateField'])->name('sections.updateField');
-
 Route::group(['prefix' => '/painel'], function(){
     Route::get('/{section}', [SectionController::class, 'show'])->name('panel.show');
     Route::get('/{section}/create', [SectionController::class, 'create'])->name('sections.create');
